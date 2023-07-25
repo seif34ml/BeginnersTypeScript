24db228c2fc918315607fbd49fa67a360e0e036e
seif1125 <seifammar1125@gmail.com>
2023-07-25 13:39:56 +0300

    on saving ts on Error no main.js generated

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,4 @@ window.alert('hello'+username);
 let a = 'a'
 let b = 70
 let c = '7'
-
-document.getElementsByTagName('body')[0].innerText = (b / c)
+document.getElementsByTagName('body')[0].innerText =(b/c)
