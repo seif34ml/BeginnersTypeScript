1f7483f81c2447400802b79e60df79ef9a44ae10
seif1125 <seifammar1125@gmail.com>
2023-07-25 15:04:16 +0300

    add all possible primitive types

--- build/main.js
+++ build/main.js
@@ -1,7 +1,15 @@
 "use strict";
 let myName = 'Seif';
-let age;
+//let age:Number
 let date;
 let active;
 let albumID;
-age = 43;
+date = new Date('Tue Jul 25 2023 14:55:14 GMT+0300');
+console.log(date);
+active = false;
+albumID = true;
+albumID = 34;
+albumID = 'ss345';
+albumID = { name: 'ss' };
+albumID = [1, 2, 3, 'h'];
+console.log(albumID);
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 let myName: string = 'Seif'
-let age:Number
+//let age:Number
 let date:Date
 let active:Boolean
 let albumID:any
 
+date=new Date('Tue Jul 25 2023 14:55:14 GMT+0300');
+console.log(date);
+active=false
+albumID=true
+albumID=34
+albumID='ss345'
+albumID={name:'ss'}
+albumID=[1,2,3,'h']
+console.log(albumID)
 
-age=43
+
+
 
