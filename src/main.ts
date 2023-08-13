c7a27bd363b8639077d55f38a4b2c2248cfacd25
seif1125 <seifammar1125@gmail.com>
2023-08-13 13:00:55 +0300

    Add Functions and types

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,66 @@
-let myName: string = 'Seif'
-//let age:Number
-let date:Date
-let active:Boolean
-let albumID:any
+//type aliases
 
-date=new Date('Tue Jul 25 2023 14:55:14 GMT+0300');
-console.log(date);
-active=false
-albumID=true
-albumID=34
-albumID='ss345'
-albumID={name:'ss'}
-albumID=[1,2,3,'h']
-console.log(albumID)
-
-
-let array:[string,number,Boolean]=['string',1,true]
-
-array[0]='seif' // as array first char 0 is string typescript emits error as it is assigned as numebr
-
+type stringOrNumber=(string|number)
 
 interface Album{
  name:String,
  id:Number|String,
- albumsongs:[...any]
+ albumsongs:stringOrNumber
+};
+
+let album1:Album={
+name:'albumname',
+id:78,
+albumsongs:'hj',
+} ;
+
+
+// leteral types
+
+ let user:'DAVE'|'JOHN'|'STEVE';
+ //user='dave';                       // not related to the three hardcoded names assigned above to user
+
+user='DAVE' 
+
+
+const addWithReturnType=function (a:number,b:number):number{
+    //return a+b+'6' // will pass error as  type specified not equal type returned (string)
+    return a+b+parseInt('6') // will return sumation plus 6
 }
 
-
-
-let album:Album={
-
-    name:'seif',
-    id:'hj345',
-    albumsongs:['song 1','song 2','ff']
+const add=function (a:number,b:number){
+    //return a+b+'9' // will return a string of sumation adjacent to 6 as strin X6 as no type specified
+    return a+b+parseInt('6') // will return sumation plus 6
 }
 
+type mathOperations=(a:number,b:number)=>number;
+type voidMessage=(message:any)=>void
+// type used to specify function params types and return type
+
+
+const logMsg:voidMessage=function(message){console.log(message)}
+
+
+const addOperation:mathOperations=function(a,b){return a+b}
+
+
+logMsg(addOperation(1,2))  // output 3
+
+
+//rest parameter
+
+type Allsummations=(...numbers:number[])=>number
+// suppose we have more than two number and we want to get their sum we need to make the rest param which transfer any number of args to array
+ let getAllSum:Allsummations=function(...nums){
+  return nums.reduce((p,c)=>{
+      return c+p
+  })
+ }
+
+ logMsg(getAllSum(4,5,6,7)) //22
+
+
+ 
 
 
 
