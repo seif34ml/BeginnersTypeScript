288aae6a9bf889f4c7ce0dad1168b8fc4de68840
seif1125 <seifammar1125@gmail.com>
2023-09-12 15:41:45 +0300

    add Classes lesson

--- build/main.js
+++ build/main.js
@@ -1,4 +1,72 @@
 "use strict";
-//Assertions
-let a = 'hello';
-// let b : c='helloo'  src/main.ts:14:5 - error TS2322: Type '"helloo"' is not assignable to type '"hello"'.
+//CLASSES
+class Coder {
+    // NO NEED TO DECLARE HERE OUR VARIABLE OR VISIBLE MEMBER HERE 
+    constructor(name, // THIS IS A VARIABLE THAT IS ONLY ACCESSED THROUGH THE INSTANCE OF THECLASS
+    lang, 
+    /**  THIS IS A VARIABLE THAT IS ONLY
+     ACCESSED THROUGH THE INSTANCE OF THE CLASS AND THE CHILD CLASSES */
+    age //ACCESED EVERYWHERE
+    ) {
+        this.name = name;
+        this.lang = lang;
+        this.age = age;
+        this.name = name;
+        this.lang = lang;
+        this.age = age;
+    }
+    getLang() {
+        console.log('the lang is', this.lang);
+    }
+    setLang(lang) {
+        // this.lang=lang;  
+        /**  we will find that there is error here as readonly
+         * type WHICH WE CAN USE AS AN UNCHANFED VARIABLE */
+    }
+}
+const seif = new Coder('seif', 'typescript', 30);
+seif.getLang();
+// seif.setLang('PHP');
+// seif.getLang();
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//inheritance
+class WebDev extends Coder {
+    constructor(name, lang, age, device) {
+        super(name, lang, age); // we use super as the father constructor of parent classes
+        this.device = device;
+        this.device = device; // assif=gn argument device to device member
+    }
+    getDevice() {
+        this.typeDevice();
+    }
+    typeDevice() {
+        console.log('device is', this.device);
+    }
+}
+const seifdev = new WebDev('seif', 'javascript', 30, 'dell');
+seifdev.getLang();
+console.log(seifdev.age);
+//console.log(seifdev.lang);//  as it is a private variable it is impossible to access directly
+//seifdev.typeDevice();// this is also private cannot be accessed directly
+seifdev.getDevice();
+class Dog {
+    constructor(name, sound) {
+        this.name = name;
+        this.sound = sound;
+    }
+    playSound() {
+        return `hello i am dog ,my name is ${this.name} and my sound is ${this.sound}`;
+    }
+}
+class Cat {
+    constructor(name, sound) {
+        this.name = name;
+        this.sound = sound;
+    }
+    playSound() {
+        return `hello i am cat ,my name is ${this.name} and my sound is ${this.sound}`;
+    }
+}
+const mylo = new Dog('MYLO', 'WOOF WOOF');
+const jef = new Cat('jeff', 'meow meow');
+console.log(mylo.playSound(), jef.playSound());
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,126 @@
-//Assertions
 
 
-type a=String|Number
-type b = String
-type c = 'hello'
+//CLASSES
+
+class Coder {
+   // NO NEED TO DECLARE HERE OUR VARIABLE OR VISIBLE MEMBER HERE 
+    constructor(
+        private name:String, // THIS IS A VARIABLE THAT IS ONLY ACCESSED THROUGH THE INSTANCE OF THECLASS
+       
+        protected readonly lang:String, 
+        /**  THIS IS A VARIABLE THAT IS ONLY
+         ACCESSED THROUGH THE INSTANCE OF THE CLASS AND THE CHILD CLASSES */
+
+        public age:Number //ACCESED EVERYWHERE
+    )
+    {
+        this.name=name;
+        this.lang=lang;
+        this.age=age;
+    }
+
+
+    public getLang(){
+         console.log('the lang is' ,this.lang);
+    }
+    public setLang(lang:String){
+        // this.lang=lang;  
+        /**  we will find that there is error here as readonly  
+         * type WHICH WE CAN USE AS AN UNCHANFED VARIABLE */
+    }
 
 
 
 
-
-let a : c='hello'
-
-// let b : c='helloo'  src/main.ts:14:5 - error TS2322: Type '"helloo"' is not assignable to type '"hello"'.
+}
 
 
+const seif=new Coder('seif','typescript',30)
 
+seif.getLang();
+// seif.setLang('PHP');
+// seif.getLang();
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//inheritance
+class WebDev extends Coder{
+
+    constructor(
+       name:String, 
+      lang:String, 
+       age:Number,
+        public device:String 
+    ){
+    super(name,lang,age) // we use super as the father constructor of parent classes
+      this.device=device // assif=gn argument device to device member
+    }
+
+    public getDevice(){
+        this.typeDevice()
+    }
+
+    private typeDevice(){
+        console.log('device is',this.device);
+    }
+}
+
+const seifdev=new WebDev ('seif','javascript',30,'dell')
+
+seifdev.getLang();
+console.log(seifdev.age);
+
+//console.log(seifdev.lang);//  as it is a private variable it is impossible to access directly
+//seifdev.typeDevice();// this is also private cannot be accessed directly
+
+seifdev.getDevice()
+//this function contain the type device private function but her the function is accessible as we accessed it from a public function inside class 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  
 
+//interface
+
+interface Sound{
+
+    name:String,
+    sound:String,
+    playSound():String
+
+}
 
 
+
+class Dog implements Sound{
+    name:string
+    sound:string
+    constructor( name:string,
+        sound:string){
+         this.name=name
+         this.sound=sound
+    }
+
+    playSound(): String {
+        return `hello i am dog ,my name is ${this.name} and my sound is ${this.sound}`
+    }
+}
+
+class Cat implements Sound{
+    name:string
+    sound:string
+    constructor( name:string,
+        sound:string){
+         this.name=name
+         this.sound=sound
+    }
+
+    playSound(): String {
+        return `hello i am cat ,my name is ${this.name} and my sound is ${this.sound}`
+    }
+}
+const mylo=new Dog('MYLO','WOOF WOOF')
+const jef=new Cat('jeff','meow meow')
+
+
+
+console.log(mylo.playSound(),jef.playSound());
